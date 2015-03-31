c12c152ba4b796cf2d695019237583a4dfe75a1a
dinnoc <james.dinniss@gmail.com>
2015-03-31 13:58:30 +0100

    Initial Commit

---
+++ DMSPollingService/DMSPollingModule.vb
@@ -0,0 +1,516 @@
+﻿Imports System.ServiceProcess
+Imports System.Configuration.Install
+Imports System.Diagnostics
+Imports System.Configuration
+Imports NationalInstruments.DAQmx
+Imports System.Threading
+Imports System.IO
+
+Public Class NewService1
+    Inherits System.ServiceProcess.ServiceBase
+    Friend WithEvents Timer1 As System.Timers.Timer
+    Private newtask As New Task
+    Private newdigreader As DigitalSingleChannelReader
+    Private newanalogreader As AnalogSingleChannelReader
+
+    Private HttpReaders As New System.Collections.Specialized.ListDictionary()
+
+    Public arrDAQ(10, 17) As Object
+    Public cycle As Integer
+    Public Sub New()
+        MyBase.New()
+        InitializeComponents()
+        ' TODO: Add any further initialization code
+    End Sub
+
+    Private Sub InitializeComponents()
+        Me.ServiceName = "DMSPollingService"
+        Me.AutoLog = True
+        Me.CanStop = True
+        Me.Timer1 = New System.Timers.Timer()
+        Me.Timer1.Interval = 5000
+        Me.Timer1.Enabled = True
+
+    End Sub
+    ' This method starts the service.
+    <MTAThread()> Shared Sub Main()
+        Try
+
+            ' To run more than one service you have to add them to the array
+            System.ServiceProcess.ServiceBase.Run(New System.ServiceProcess.ServiceBase() _
+            {New NewService1})
+
+        Catch ex As Exception
+            Dim sw As StreamWriter
+            sw = File.AppendText("c:/DAQServiceErrors.txt")
+            sw.WriteLine(Now() & "--MainInit--" & ex.Message)
+            sw.Close()
+        End Try
+    End Sub
+    ' Clean up any resources being used.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        MyBase.Dispose(disposing)
+        ' TODO: Add cleanup code here (if required)
+    End Sub
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' TODO: Add start code here (if required)
+        ' to start your service.
+        Call DataLoad()
+
+        Try
+
+            Dim MyLog As New EventLog() ' create a new event log 
+            ' Check if the the Event Log Exists 
+
+            'If Not MyLog.SourceExists("DMSPollingService") Then
+            'MyLog.CreateEventSource("DMSPollingService", "DMSPollingService1 Log") ' Create Log 
+            'End If
+            If Not EventLog.SourceExists("DMSPollingService") Then
+                EventLog.CreateEventSource("DMSPollingService", "DMSPollingService1 Log") ' Create Log 
+            End If
+
+            MyLog.Source = "DMSPollingService"
+            'MyLog.WriteEntry("DMSPollingService1 Log", "It is running v2", EventLogEntryType.Information)
+            EventLog.WriteEntry("DMSPollingService1 Log", "It is running v2", EventLogEntryType.Information)
+
+        Catch ex As Exception
+
+            ErrorWriter("Error handling init error", ex)
+
+        End Try
+
+        Me.Timer1.Enabled = True
+
+    End Sub
+    Protected Overrides Sub OnStop()
+        ' TODO: Add tear-down code here (if required) 
+        ' to stop your service.
+        Me.Timer1.Enabled = False
+    End Sub
+    Private Sub InitializeComponent()
+        'LogWriter("initializecomponent")
+
+        Me.Timer1 = New System.Timers.Timer
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).BeginInit()
+
+        Me.Timer1.Enabled = True
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).EndInit()
+
+    End Sub
+    Sub DataLoad()
+        'LogWriter("data load")
+
+        Try
+
+            Dim adapdevice As New DeviceTableAdapters.tblEventDeviceTableAdapter
+            Dim dtdevice As New Device.tblEventDeviceDataTable
+
+            dtdevice = adapdevice.GetData()
+
+            Dim n As Integer
+
+            ' TODO: use constant names instead of numbers
+            ' will be easier to read and write the code
+            ' declare C_DEVICE_PATH as 1
+            ' and use arrDAQ(n, C_DEVICE_PATH) instead of arrDAQ(n, 1)
+
+            ' TODO: give brief description for each ite, like 'row' or 'DeviceType'
+
+            '1 path
+            '2 digital reader
+            '3 value
+            '4 locid
+            '5 row in downtime db that its written to
+            '6 low - also used for location of httpstring for http device
+            '7 lowtol
+            '8 high
+            '9 hightol
+            '10 highorlow
+            '11 DeviceID
+            '12 Samples read
+            '13 Total for samples read
+            '14 Device Type (analog or digital or counter)
+            '15 Thread Running for this device boolean
+            '16 Counter Value
+            '17 Connection Up? True = Good Connection, False = no connection
+
+
+
+
+            For Each row As Device.tblEventDeviceRow In dtdevice
+                'LogWriter(">> " & row.DeviceLocID)
+                n = n + 1
+                arrDAQ(n, 1) = row.DevicePath
+                arrDAQ(n, 4) = row.DeviceLocID
+                arrDAQ(n, 5) = row.DeviceEventID
+                arrDAQ(n, 6) = row.DeviceLowVal
+                arrDAQ(n, 7) = row.DeviceLowTol
+                arrDAQ(n, 8) = row.DeviceHighVal
+                arrDAQ(n, 9) = row.DeviceHighTol
+                arrDAQ(n, 10) = row.DeviceStatus
+                arrDAQ(n, 11) = row.EventDeviceID
+                arrDAQ(n, 12) = 0
+                arrDAQ(n, 13) = 0
+                arrDAQ(n, 14) = row.DeviceType
+                arrDAQ(n, 15) = False
+                arrDAQ(n, 16) = 0
+                arrDAQ(n, 17) = False
+
+
+            Next
+
+            For i = 1 To 10
+
+                If Not arrDAQ(i, 1) = Nothing Then
+
+                    If arrDAQ(i, 14) = 5 Then
+
+                        Dim url As String = arrDAQ(i, 1)
+                        Dim httpreader As httpPull2
+                        If HttpReaders.Contains(url) Then
+                            httpreader = HttpReaders(url)
+                        Else
+                            httpreader = New httpPull2(url)
+                            HttpReaders.Add(url, httpreader)
+                        End If
+
+                        arrDAQ(i, 2) = httpreader
+                        arrDAQ(i, 17) = True
+                        Continue For
+
+                    End If
+
+
+                    'create new task for the DAQ card
+                    newtask = New Task()
+
+                    If arrDAQ(i, 14) = 1 Then
+                        Try
+                            'set the task to read a channel on the DAQ card
+                            newtask.AIChannels.CreateVoltageChannel(arrDAQ(i, 1), "Analog", AITerminalConfiguration.Differential, 0, 5, AIVoltageUnits.Volts)
+                            'set up the reader to use the task for reading the DAQ card
+                            newanalogreader = New AnalogSingleChannelReader(newtask.Stream)
+                            'write the reader to the matrix
+                            arrDAQ(i, 2) = newanalogreader
+                            arrDAQ(i, 17) = True
+
+                        Catch ex As Exception
+                            ErrorWriter("Analog Initialisation Error", ex)
+                        End Try
+
+                    ElseIf arrDAQ(i, 14) = 2 Then
+                        'configure a digital reader
+                        Try
+                            newtask.DIChannels.CreateChannel(arrDAQ(i, 1), "Digi", ChannelLineGrouping.OneChannelForEachLine)
+                            newdigreader = New DigitalSingleChannelReader(newtask.Stream)
+                            arrDAQ(i, 2) = newdigreader
+                            arrDAQ(i, 17) = True
+                        Catch ex As Exception
+                            ErrorWriter("Digital Initialisation error:", ex)
+                        End Try
+
+                    ElseIf arrDAQ(i, 14) = 3 Then
+                        'configure a counter reader
+                        startCounterDevice(i)
+                    End If
+
+                End If
+
+            Next i
+
+            Timer1.Start()
+
+        Catch ex As Exception
+
+            ErrorWriter("General Initialisation Error", ex)
+        End Try
+    End Sub
+
+    Sub startCounterDevice(ByVal i As Integer)
+
+        Try
+
+            Dim ctTask As New Task
+            'get current count for device
+            Dim adapQuery As New DeviceTableAdapters.QueriesTableAdapter
+            Dim currentcount As Integer = adapQuery.GetCurrentCount(arrDAQ(i, 11))
+
+            ctTask.CIChannels.CreateCountEdgesChannel(arrDAQ(i, 1), "Counter", CICountEdgesActiveEdge.Rising, currentcount, CICountEdgesCountDirection.Up)
+            ctTask.CIChannels(0).CountEdgesDigitalFilterEnable = True
+            ctTask.CIChannels(0).CountEdgesDigitalFilterMinimumPulseWidth = 0.0007
+            ctTask.Start()
+
+            arrDAQ(i, 2) = ctTask
+            arrDAQ(i, 17) = True
+
+        Catch ex As Exception
+            arrDAQ(i, 17) = False
+            ErrorWriter("Counter Initialisation error:", ex)
+        End Try
+
+    End Sub
+
+    Private Sub Timer1_Elapsed(ByVal sender As System.Object, ByVal e As System.Timers.ElapsedEventArgs) Handles Timer1.Elapsed
+        startread()
+    End Sub
+
+    Sub startread()
+
+        Dim workthreads As Integer
+        Dim portthreads As Integer
+
+        Dim Threadex As Exception = New Exception("ThreadStartException")
+
+        Try
+            For i = 1 To 10
+
+                'if there is something in the first line of the matrix 
+                If Not arrDAQ(i, 1) = Nothing Then
+
+                    'if the thread is running still dont start a new one
+                    If Not arrDAQ(i, 15) Then
+
+                        If ThreadPool.QueueUserWorkItem(New WaitCallback(AddressOf DoRead), i) Then
+                            ThreadPool.GetAvailableThreads(workthreads, portthreads)
+                        Else
+                            ErrorWriter("ThreadStartFailed", Threadex)
+                        End If
+
+                    End If
+                End If
+                Thread.Sleep(100)
+            Next i
+
+            'update end time of any rows which have a status of -1 (started but not ended)
+            Dim AdapEndtime As New EventsTableAdapters.QueriesTableAdapter
+            AdapEndtime.UpdateEventWithStatusNeg1()
+            AdapEndtime.UpdateServerHandshake()
+
+        Catch ex As Exception
+            ErrorWriter("StartRead ", ex)
+        End Try
+    End Sub
+
+    Private Sub DoRead(ByVal i As Integer)
+        Try
+
+            arrDAQ(i, 15) = True
+
+            If arrDAQ(i, 14) = 5 Then
+
+                Try
+                    'LogWriter("pre getting data from " & arrDAQ(i, 4) & " res: " & arrDAQ(i, 3))
+
+                    Dim httpreader As httpPull2 = arrDAQ(i, 2)
+                    arrDAQ(i, 3) = httpreader.getCachedData(arrDAQ(i, 6))
+
+                    If System.Configuration.ConfigurationManager.AppSettings("logOutput").ToString() = "True" Then
+                        LogWriter("getting data from " & arrDAQ(i, 4) & " res: " & arrDAQ(i, 3) & " location: " & arrDAQ(i, 6))
+                    End If
+
+                    If arrDAQ(i, 3) = 1 Then
+
+                        endevent(i)
+
+                    ElseIf arrDAQ(i, 3) = 0 Then
+
+                        startevent(i)
+
+                    End If
+
+
+                Catch ex As Net.WebException
+                    ErrorWriter("couldn't receive data from server: " & arrDAQ(i, 1) & ": ", ex)
+
+                Catch ex As IndexOutOfRangeException
+                    ErrorWriter("returned data from " & arrDAQ(i, 1) & " is invalid", ex)
+
+                Catch ex As Exception
+                    ErrorWriter("cannot obtain information from: " & arrDAQ(i, 11) & "@" & arrDAQ(i, 4), ex)
+
+                End Try
+
+            End If
+
+            If arrDAQ(i, 14) = 2 Then  'if its a digital read
+
+                arrDAQ(i, 3) = CInt(arrDAQ(i, 2).readsinglesamplesingleline())
+
+                If arrDAQ(i, 3) = -1 Then
+
+                    endevent(i)
+
+                ElseIf arrDAQ(i, 3) = 0 Then
+
+                    startevent(i)
+
+                End If
+
+            End If
+
+            If arrDAQ(i, 14) = 3 Then ' if its a counter
+
+                Dim oldCount As Integer = arrDAQ(i, 16)
+
+                Try
+
+                    'if device has lost communication then try to restart the device at each read
+                    If arrDAQ(i, 17) = False Then
+                        Call startCounterDevice(i)
+                    Else
+
+                        'read the value of the counter
+                        arrDAQ(i, 16) = CInt(arrDAQ(i, 2).CIChannels(0).Count)
+
+                        'if the counter is the same as last value the line is stopped
+                        If arrDAQ(i, 16) <= oldCount Then
+                            startevent(i)
+
+                            'if the counter has incremented since the last value
+                        ElseIf arrDAQ(i, 16) > oldCount Then
+                            endevent(i)
+                        End If
+
+                        'update the asset table with the current count
+                        Dim adapEvent As New EventsTableAdapters.QueriesTableAdapter
+                        adapEvent.UpdateEventDeviceCounter(arrDAQ(i, 16), arrDAQ(i, 11))
+
+                    End If
+                Catch ex As Exception
+
+                    Dim str As String = ex.Message
+                    ErrorWriter("Counter Count Error", ex)
+
+                    If str.Contains("-50405") Or str.Contains("-200088") Then
+                        arrDAQ(i, 17) = False
+                    End If
+
+                End Try
+
+
+            End If
+
+
+
+            If arrDAQ(i, 14) = 4 Then ' if its a counter-only device (ie: digital device does start and stop)
+
+                Try
+                    Dim sw As StreamWriter
+                    sw = File.AppendText("c:/DAQServiceErrors.txt")
+
+
+                    'if device has lost communication then try to restart the device at each read
+                    If arrDAQ(i, 17) = False Then
+                        Call startCounterDevice(i)
+                    Else
+
+                        'read the value of the counter
+                        arrDAQ(i, 16) = CInt(arrDAQ(i, 2).CIChannels(0).Count)
+
+                        'update the asset table with the current count
+                        Dim adapEvent As New EventsTableAdapters.QueriesTableAdapter
+                        adapEvent.UpdateEventDeviceCounter(arrDAQ(i, 16), arrDAQ(i, 11))
+
+                        sw.WriteLine(arrDAQ(i, 17), "--", arrDAQ(i, 2), arrDAQ(i, 16))
+
+
+                    End If
+                    sw.Close()
+                Catch ex As Exception
+
+                    Dim str As String = ex.Message
+                    ErrorWriter("Counter-only Count Error", ex)
+
+                    If str.Contains("-50405") Or str.Contains("-200088") Then
+                        arrDAQ(i, 17) = False
+                    End If
+
+                End Try
+
+
+            End If
+
+
+            arrDAQ(i, 15) = False
+
+        Catch ex As Exception
+
+            ErrorWriter("DoRead: " & arrDAQ(i, 1), ex)
+            arrDAQ(i, 15) = False
+
+        End Try
+
+    End Sub
+
+    Sub startevent(ByVal i As Integer)
+        Dim newrow As Integer
+        Dim AdapEvent As New EventsTableAdapters.QueriesTableAdapter
+
+        If arrDAQ(i, 10) = False Then
+            AdapEvent.InsertStartEvent(Now(), arrDAQ(i, 4), arrDAQ(i, 11), 0, -1, arrDAQ(i, 16), newrow)
+            AdapEvent.udpateTblDevice(True, newrow, arrDAQ(i, 11))
+            arrDAQ(i, 10) = True
+            arrDAQ(i, 5) = newrow
+        End If
+
+    End Sub
+
+    Sub endevent(ByVal i As Integer)
+
+        If arrDAQ(i, 10) = True Then
+            Dim AdapEvent As New EventsTableAdapters.QueriesTableAdapter
+            AdapEvent.InsertEndEvent(Now(), 0, arrDAQ(i, 16), arrDAQ(i, 5))
+            AdapEvent.udpateTblDevice(False, 0, arrDAQ(i, 11))
+            arrDAQ(i, 10) = False
+            arrDAQ(i, 5) = 0
+        End If
+
+    End Sub
+    Shared Sub ErrorWriter(ByVal pretext As String, ByVal ex As Exception)
+
+        'INSERT A CATCH PHRASE HERE TO SEE IF IT ELIMINATES THE IO ERROR BEING GENERATED BY THIS SERVICE JD 100809
+        Try
+            Dim sw As StreamWriter
+            sw = File.AppendText("c:/DAQServiceErrors.txt")
+            sw.WriteLine(Now() & "--" & pretext & "--" & ex.Message)
+            sw.Close()
+
+        Catch exc As Exception
+
+        End Try
+    End Sub
+
+    Shared Sub LogWriter(ByVal pretext As String)
+
+        'log some debug messages
+        Try
+            Dim sw As StreamWriter
+            sw = File.AppendText("c:/DAQServiceLogs.txt")
+            sw.WriteLine(Now() & "--" & pretext & "--")
+            sw.Close()
+
+        Catch exc As Exception
+
+        End Try
+    End Sub
+
+End Class
+
+
+Public Class httpPull2
+    Inherits httpPull
+
+    Public Sub New(ByVal str As String)
+        MyBase.New(str)
+    End Sub
+
+    ' overrides logger so the information can be saved in files
+    Public Overrides Sub logerr(ByVal pretext As String, ByVal ex As Exception)
+        NewService1.ErrorWriter(pretext, ex)
+    End Sub
+
+    Public Overrides Sub log(ByVal pretext As String)
+        NewService1.LogWriter(pretext)
+    End Sub
+
+End Class
---
+++ DMSPollingService/Httppull.vb
@@ -0,0 +1,209 @@
+﻿Imports System.Threading
+Imports System.IO
+
+Public Class httpPull
+
+    ' url from which data is being pulled of
+    Private Property _url As String
+    ' array holding parsed reply from server
+    Private Property _result As List(Of Integer)
+    Private Property _cached = False
+    Private Property _semaphore As Semaphore = New Semaphore(0, 1)
+
+    Public Sub New(ByVal url As String)
+
+        _url = url
+        _result = New List(Of Integer)
+        _semaphore.Release(1)
+
+    End Sub
+
+    ' connects to a server and returns response
+    Private Function sendRequest(ByVal address As String)
+
+        Dim webClient As New System.Net.WebClient()
+        Return webClient.DownloadString(address)
+
+    End Function
+
+#Region "Obsolete Code"
+
+
+
+    ' transforms input: 00 01 01 into array of integers 0,1,1
+    'Private Function parseResult(ByVal input As String)
+    '    Try
+
+
+    '        ' Dim result As List(Of Integer) = New List(Of Integer)
+    '        'Console.WriteLine("input: {0} length: {1}", input, input.Length())
+    '        'log("input: " & input & " length: " & input.Length())
+    '        Dim idx As Integer = 0
+
+    '        Dim strResult() As String = input.Split(" ")
+    '        Dim intResult() = Array.ConvertAll(strResult, Function(str) Int32.Parse(str))
+
+    '        Dim result = intResult.ToList()
+
+    '        log("response " & input.ToString & "- Length" & input.Length & "-" & result(0) & "-" & result(1))
+
+    '        ' get the number of triplets from the lenfth of response 
+    '        ' the first block's length is 2, so it needs to be incremented
+    '        'Dim dbl_blocks As Double = (input.Length() + 1) / 3
+    '        'Dim int_blocks As Integer = (input.Length() + 1) / 3
+
+    '        '' compare result of flowting point and integer division
+    '        '' to find out if the input is valid
+    '        'If Not dbl_blocks = int_blocks Then
+    '        '    log("invalid length of http response. this was the response " & input.ToString & "- Length" & input.Length)
+    '        '    Return result
+    '        'End If
+
+
+    '        'For index As Integer = 1 To input.Length() Step 3
+    '        '    'Console.WriteLine("id[{0}] res: {1} ", index, input.ElementAt(index))
+    '        '    result.Add(Val(input.ElementAt(index)))
+    '        'Next
+
+    '        ' other idea, maybe even better
+    '        ' split by ' ' and then make sure each has length of 2 bytes
+    '        'Dim data As List(Of String) = input.Split(" ")
+
+    '        Return result
+    '    Catch ex As Exception
+    '        ErrorWriter("Parse Error", ex)
+    '    End Try
+
+    'End Function
+#End Region
+
+    ' transforms input: 00 01 01 into array of integers 0,1,1
+    Private Function parseResult(ByVal input As String)
+        Try
+            'trim whitespace off string, also lots of CRLFs at end of string
+            input = input.Trim()
+            'split string at spaces
+            Dim strResult() As String = input.Split(" ")
+            'take only the right hand value of the pair
+            Dim strResultTrim() = Array.ConvertAll(strResult, Function(str) Right(Left(str, 2), 1))
+            'convert array to integer array from string
+            Dim intResult() = Array.ConvertAll(strResultTrim, Function(str) Int32.Parse(str))
+            'convert array to list type
+            Dim result = intResult.ToList()
+
+            Return result
+
+        Catch ex As Exception
+            ErrorWriter("Parse Error", ex)
+
+        End Try
+
+    End Function
+
+    ' gets information about line status from cache
+    ' unles it is out of date or needs to be initialised, then it refreshes cache
+    Public Function getCachedData(ByVal index As Integer)
+
+        ' obtain exclusive lock to avoid race condition situation
+        'log("obtaining lock")
+        _semaphore.WaitOne()
+        'log("got lock")
+        Dim result As Integer = -1
+
+        'because the the code here is executing in critical section 
+        'all exceptions have to be cought, semafor then has to be released 
+        'and exception re-thrown
+        Try
+            ' if data is cached get result from it
+            ' can throw index error exception
+            If _cached Then
+                result = _result(index)
+            End If
+
+            ' the data is out of date or the data was never cached refresh it
+            ' can throw any network exception
+            If result = -1 Or Not _cached Then
+                _result = updateCache()
+                'get result again
+                'can throw index error
+                result = _result(index)
+            End If
+
+            ' make sure next time we get data refreshed
+            _result(index) = -1
+
+            'catch exception, release semaphore and re-throw exception
+        Catch ex As Exception
+            _semaphore.Release(1)
+            Throw ex
+        End Try
+
+        _cached = True
+
+        ' leave critical section
+        _semaphore.Release(1)
+        'log("released lock")
+        Return result
+
+    End Function
+
+    ' updates cached data
+    Private Function updateCache()
+
+        Dim httpresult As String
+
+        ' get data from a server
+        httpresult = sendRequest(_url)
+        'Console.WriteLine("http result: " & httpresult)
+
+        'Commented out as used for testing only
+        'Dim ex As New Exception
+        'ErrorWriter("HttpString: " & httpresult, ex)
+
+        ' parse it and return result
+        Return parseResult(httpresult)
+
+    End Function
+
+    ' two loggers, for logging information, and errors
+
+    Public Overridable Sub log(ByVal message As String)
+
+        Console.WriteLine("info: " & message)
+
+    End Sub
+
+    Public Overridable Sub logerr(ByVal message As String, ByVal ex As Exception)
+
+        Console.WriteLine(message & ": " & ex.Message)
+
+    End Sub
+
+    ' small unit test
+    Public Sub UTparse(ByVal input As String)
+
+        Console.WriteLine("input: '{0}'", input)
+        Dim result As List(Of Integer) = parseResult(input)
+
+        Dim idx As Integer = 0
+        For Each x As Integer In result
+            Console.WriteLine("res[{0}]: {1}", idx, x)
+            idx += 1
+        Next
+    End Sub
+
+    Shared Sub ErrorWriter(ByVal pretext As String, ByVal ex As Exception)
+
+        'INSERT A CATCH PHRASE HERE TO SEE IF IT ELIMINATES THE IO ERROR BEING GENERATED BY THIS SERVICE JD 100809
+        Try
+            Dim sw As StreamWriter
+            sw = File.AppendText("c:/DAQServiceErrors.txt")
+            sw.WriteLine(Now() & "--" & pretext & "--" & ex.Message)
+            sw.Close()
+
+        Catch exc As Exception
+
+        End Try
+    End Sub
+
+End Class
